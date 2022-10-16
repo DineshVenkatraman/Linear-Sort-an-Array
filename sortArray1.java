d4bec925612a3ef9fa46b5f4299926af0ba19060
Dinesh Venkatraman <114977306+DineshVenkatraman@users.noreply.github.com>
2022-10-16 12:32:42 +0530

    Sort an array as linear sort
    to sort the value as linear Sort -if any small value to change or swapping the index location as ascending order using linear sort

---
+++ sortArray1.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+class sortArr
+{
+    Scanner sc=new Scanner(System.in);
+    public void sorta1()
+    {
+        int i,j,n,temp;
+        System.out.println("Enter the  no of Elements ");
+        n=sc.nextInt();
+        int a[]=new int[n];
+        System.out.println("Enter the Elements");
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+
+        for (i=0;i<n-1;i++)
+        {
+            for (j=i+1;j<n;j++)
+            {
+                if(a[i]>a[j])
+                {
+                    temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }
+            }
+        }
+        System.out.println("Sorted array list");
+        for(i=0;i<n;i++)
+        {
+            System.out.print(a[i]+" , ");
+        }
+    }
+}
+
+
+
+
+public class sortArray1 {
+    public static void main(String args[])
+    {
+        sortArr objArr=new sortArr();
+        objArr.sorta1();
+    }
+}
